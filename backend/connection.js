65a71190d86202f81d8a5e9120c32adac86dae94
Alec Hannaford <alec322@gmail.com>
2024-04-04 13:21:46 -0700

    connection

---
+++ backend/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb://mongo:27017/AlecTodoAppTest"
+
+mongoose.connect(DATABASE_URL)
+
+mongoose.connection
+    .on("open", () => console.log(`CONNECTED TO MONGOOSE : ${DATABASE_URL}`))
+    .on("close", () => console.log(`CLOSED CONNECTION! : ${DATABASE_URL}`))
+    .on("error", (error) => console.log({DATABASE_URL : error}))
+
+module.exports = mongoose
